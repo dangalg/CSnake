5b6e762fb386c6fb641f6e5fc95ebadd2ce8ad4e
dangalg <dangalg@gmail.com>
2021-08-23 22:11:05 +0300

    added goto xy

--- CSnake/Snake.c
+++ CSnake/Snake.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <conio.h>
+#include <windows.h> 
 
-#define SCREEN_WIDTH 60
+
+#define SCREEN_WIDTH 30
 #define SCREEN_HEIGHT 25
 
 void UserInput();
 void Update();
 void Draw();
 void MoveSnake(int direction);
+void gotoxy(int x, int y);
 
 char userInput;
-char screen[SCREEN_WIDTH][SCREEN_HEIGHT];
 int keyCode;
 bool gameRunning = true;
 
+int borders[SCREEN_WIDTH][SCREEN_HEIGHT];
+int snakePosition[SCREEN_WIDTH][SCREEN_HEIGHT];
+int plumsPositions[SCREEN_WIDTH][SCREEN_HEIGHT];
 
 enum{ KEY_ESC = 27, KEY_LEFT = 75, KEY_RIGHT = 77, KEY_UP = 72, KEY_DOWN = 80 };
 enum{LEFT = 0, RIGHT = 1, UP = 2, DOWN = 3};
@@ -68,41 +73,51 @@ void Update()
 	{
 		for (size_t y = 0; y < SCREEN_WIDTH; y++)
 		{
-			// convert index to char
-			itoa(y, index, 10);
-
-			// print index
-			printf("%s", index);
-
+			// set borders
+			if(x == 0 || x == SCREEN_HEIGHT - 1)
+			{
+				borders[x][y] = 1;
+			}
 		}
-		printf("\n");
 	}
 	
 }
 
 void Draw()
 {
-	system("cls");
-
-	char index[3] = { "0" };
+	//system("cls");
 
 	// print screen matrix
 	for (size_t x = 0; x < SCREEN_HEIGHT; x++)
 	{
 		for (size_t y = 0; y < SCREEN_WIDTH; y++)
 		{
-			// convert index to char
-			itoa(y, index, 10);
+			// move cursor to console position
+			gotoxy(y, x);
 
-			// print index
-			printf("%s", index);
-			printf("%s", screen[x][y]);
+			// print borders
+			if (borders[x][y] == 1)
+			{
+				printf("%c", '*');
+			}
+			
+			//printf("%c", borders[x][y]);
+
+			// print snake
+			if (snakePosition[x][y] == 1) 
+			{
+				printf("%c", 'X');
+			}
+
+			// print plums
+			if (plumsPositions[x][y] == 1)
+			{
+				printf("%c", 'O');
+			}
 
 		}
 		printf("\n");
 	}
-	
-	
 }
 
 void MoveSnake(int direction) 
@@ -124,4 +139,12 @@ void MoveSnake(int direction)
 			break;
 	}
 
+}
+
+void gotoxy(int x, int y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
